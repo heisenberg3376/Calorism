5fa21262cb802af2877052b13335ca7635ae3233
Phanendra Sai Ram <125650236+heisenberg3376@users.noreply.github.com>
2024-04-11 13:57:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import os
 from dotenv import load_dotenv
 from PIL import Image
 
-
-
 load_dotenv() 
+
+
 
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 
@@ -42,10 +42,14 @@ if up_file is not None:
     image = Image.open(up_file)
     st.image(image, caption='Uploaded Image',use_column_width=True)
     
-submit = st.button('Tell me the Calorie Breakdown')
+c1, c2 = st.columns(2)
 
+with c1:
+    submit = st.button('Tell me the Calorie Breakdown')
+with c2:
+    cook = st.button('Know the Recipe')
 
-input_prompt = """
+input_prompt1 = """
 You are an expert nutritionist where you need to see the food items from the image and 
 calculate the total calories, also provide the details of every food items
 with calories intake in below format
@@ -58,8 +62,29 @@ with calories intake in below format
 Finally you can also mention whether the food is healthy or not in 2 lines.
 
 """
+
+input_prompt2 = """
+You are a professional chef where you need to see the food items from the given image and 
+provide a step by step recipe for cooking it. Also with the required Ingredients. In below format
+
+Required Items:
+    1. Item1
+    2. Item2
+    --------
+Recipe:
+    1. step 1
+    2. step 2
+    ---------
+
+"""
 if submit:
-    image_data = image_setup(up_file)
-    response = get_response(input_prompt=input_prompt, img=image_data)
-    st.header('Your Response is')
-    st.write(response)
+    image_data1 = image_setup(up_file)
+    response1 = get_response(input_prompt=input_prompt1, img=image_data1)
+    st.header('Calorie Breakdown')
+    st.write(response1)
+    
+if cook:
+    image_data1 = image_setup(up_file)
+    response2 = get_response(input_prompt=input_prompt2, img=image_data1)
+    st.header('Here is the Recipe')
+    st.write(response2)
